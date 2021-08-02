21375f56f8f7cd4243b24d2c7afdec7772e5c24d
Takato Okudo <okudo@nii.ac.jp>
2021-08-02 19:20:59 +0900

    Achiever becomes out.

--- shaner/aggregater/subgoal_based.py
+++ shaner/aggregater/subgoal_based.py
@@ -1,40 +1,45 @@
 import logging
 
 from shaner.aggregater.core import AbstractAggregater
-from shaner.aggregater.entity.achiever import \
-    FetchPickAndPlaceAchiever, \
-    PinballAchiever, \
-    FourroomsAchiever, \
-    CrowdSimAchiever
+# from shaner.aggregater.entity.achiever import \
+#     FetchPickAndPlaceAchiever, \
+#     PinballAchiever, \
+#     FourroomsAchiever, \
+#     CrowdSimAchiever
 
 
 logger = logging.getLogger(__name__)
 
 
-id2achiever = {
-    "SingleFetchPickAndPlace-v0": FetchPickAndPlaceAchiever,
-    "FetchPickAndPlace-v1": FetchPickAndPlaceAchiever,
-    "PinBall-v0": PinballAchiever,
-    "Pinball-Subgoal-v0": PinballAchiever,
-    "Fourrooms-v0": FourroomsAchiever,
-    "ConstFourrooms-v0": FourroomsAchiever,
-    "DiagonalFourrooms-v0": FourroomsAchiever,
-    "DiagonalPartialFourrooms-v0": FourroomsAchiever,
-    "CrowdSim-v0": CrowdSimAchiever
-}
+# 各ドメインで記述
+# id2achiever = {
+#     "SingleFetchPickAndPlace-v0": FetchPickAndPlaceAchiever,
+#     "FetchPickAndPlace-v1": FetchPickAndPlaceAchiever,
+#     "PinBall-v0": PinballAchiever,
+#     "Pinball-Subgoal-v0": PinballAchiever,
+#     "Fourrooms-v0": FourroomsAchiever,
+#     "ConstFourrooms-v0": FourroomsAchiever,
+#     "DiagonalFourrooms-v0": FourroomsAchiever,
+#     "DiagonalPartialFourrooms-v0": FourroomsAchiever,
+#     "CrowdSim-v0": CrowdSimAchiever
+# }
 
 
+# 2021/8/2
+# TODO achieverを渡すように修正したので関連プログラムは要修正
+# ドメインに依るのでachieverの作成はユーザに任せる。
 class DTA(AbstractAggregater):
-    def __init__(self, env_id, n_obs, _range, **params):
-        self.achiever = id2achiever[env_id](n_obs=n_obs, _range=_range,
-                                            **params)
+    def __init__(self, achiever, **params):
+        self.achiever = achiever
         self.current_state = 0
         self.n_states = len(self.achiever.subgoals) + 1
 
     def __call__(self, obs):
         if self.achiever.eval(obs, self.current_state):
             self.current_state += 1
-            logger.debug("Subgoal is achieved! Transit to {}".format(self.current_state))
+            logger.debug(
+                "Subgoal is achieved! Transit to {}".format(self.current_state)
+            )
             return self.current_state
         else:
             return self.current_state
@@ -47,16 +52,19 @@ class DTA(AbstractAggregater):
 
 
 class Checker(AbstractAggregater):
-    def __init__(self, env_id, n_obs, _range, **params):
-        self.achiever = id2achiever[env_id](n_obs=n_obs, _range=_range,
-                                            **params)
+    def __init__(self, achiever, **params):
+        self.achiever = achiever
+        # id2achiever[env_id](n_obs=n_obs, _range=_range,
+        #                                     **params)
         self.current_state = 0
         self.n_states = len(self.achiever.subgoals) + 1
-    
+
     def __call__(self, obs):
         if self.achiever.eval(obs, self.current_state):
             self.current_state += 1
-            logger.debug("Subgoal is achieved! Transit to {}".format(self.current_state))
+            logger.debug(
+                "Subgoal is achieved! Transit to {}".format(self.current_state)
+            )
             return True
         else:
             return False
